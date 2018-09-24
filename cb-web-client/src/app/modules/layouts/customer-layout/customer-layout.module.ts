4af44a0c424a37f24a7ab426d4f200b69bb19bcd
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-09-24 21:28:10 +0300

    WC-1 - Add UI changes on the restaurant map page

--- cb-web-client/src/app/modules/layouts/customer-layout/customer-layout.module.ts
+++ cb-web-client/src/app/modules/layouts/customer-layout/customer-layout.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CustomerLayoutRoutingModule } from './customer-layout-routing.module';
 import { MaterialModule } from './../../shared/material/material.module';
-
+import { FooterModule } from '../../components/footer/footer.module';
 import { CustomerLayoutComponent } from './customer-layout.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CustomerLayoutRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FooterModule
   ],
   declarations: [
     CustomerLayoutComponent
