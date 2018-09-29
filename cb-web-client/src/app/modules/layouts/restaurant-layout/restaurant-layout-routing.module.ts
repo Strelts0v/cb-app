5048bb95c94d9d70247b8cc6e445c527fd1c3acc
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-09-30 00:54:38 +0300

    WC-1 - Complete basic structure of restaurant layout

--- cb-web-client/src/app/modules/layouts/restaurant-layout/restaurant-layout-routing.module.ts
+++ cb-web-client/src/app/modules/layouts/restaurant-layout/restaurant-layout-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RestaurantLayoutComponent } from './restaurant-layout.component';
+import { RestaurantStates } from '../../../constants/restaurant-states.constant';
+import { RestaurantSettingsComponent } from '../../pages/restaurant-settings/restaurant-settings.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: RestaurantLayoutComponent
+    component: RestaurantLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: RestaurantStates.RESTAURANT_SETTINGS
+      },
+      {
+        path: RestaurantStates.RESTAURANT_SETTINGS,
+        component: RestaurantSettingsComponent,
+      }
+    ]
   }
 ];
 
--- cb-web-client/src/app/modules/layouts/restaurant-layout/restaurant-layout.module.ts
+++ cb-web-client/src/app/modules/layouts/restaurant-layout/restaurant-layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantLayoutRoutingModule } from './restaurant-layout-routing.module';
 import { RestaurantLayoutComponent } from './restaurant-layout.component';
+import { RestaurantSettingsModule } from '../../pages/restaurant-settings/restaurant-settings.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RestaurantLayoutRoutingModule
+    RestaurantLayoutRoutingModule,
+    RestaurantSettingsModule
   ],
   declarations: [
     RestaurantLayoutComponent
