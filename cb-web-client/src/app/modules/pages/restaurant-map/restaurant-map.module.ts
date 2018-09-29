1b2642809bae55ec7eddd3ac0c7ede92502aae89
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-09-30 00:46:57 +0300

    WC-1 - Remove routing module from restaurant map page

--- cb-web-client/src/app/modules/pages/restaurant-map/restaurant-map.module.ts
+++ cb-web-client/src/app/modules/pages/restaurant-map/restaurant-map.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AgmCoreModule } from '@agm/core';
 import { GoogleMapsAPIWrapper } from '@agm/core/services';
 import { FormsModule } from '@angular/forms';
-import { RestaurantMapRoutingModule } from './restaurant-map-routing.module';
 import { MaterialModule } from './../../shared/material/material.module';
 import { RestaurantMapComponent } from './restaurant-map.component';
 import { MapSearchFormComponent } from './map-search-form/map-search-form.component';
@@ -12,7 +11,6 @@ import { ServicesModule } from './../../../services/services.module';
 @NgModule({
   imports: [
     CommonModule,
-    RestaurantMapRoutingModule,
     MaterialModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAfJTVKnpLl0ULuuwDuix-9ANpyQhP6mfc'
