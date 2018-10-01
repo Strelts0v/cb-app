0e86064c3bd18b854f84edbfbc532143f8155577
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-01 23:04:46 +0300

    WC-3 - Add in memory data service for restaurants and organizations

---
+++ cb-web-client/src/app/services/in-memory/in-memory-data.service.ts
@@ -0,0 +1,357 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Restaurant } from '../../data/restaurant';
+import { Organization } from '../../data/organization';
+
+declare var google: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+
+    // Organization collection
+    const organizations: Organization[] = [
+      {
+        id: 1,
+        name: 'McDonalds',
+        description: 'Fast food restaurant',
+        icon: {
+          url: '/assets/restaurants-logo/mcdonalds.png',
+          scaledSize: {
+            width: 30,
+            height: 30,
+          }
+        },
+      },
+      {
+        id: 1,
+        name: 'KFC',
+        description: 'Fast food restaurant',
+        icon: {
+          url: '/assets/restaurants-logo/kfc.png',
+          scaledSize: {
+            width: 30,
+            height: 30,
+          }
+        },
+      }
+    ];
+
+    // Restaurants collection
+    const restaurants: Restaurant[] = [
+      {
+        id: 1,
+        organizationId: 2,
+        name: 'KFC',
+        location: {
+          lat: 53.9147,
+          lng: 27.5680,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9147,
+            lng: 27.5680,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/kfc.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'KFC'
+          }
+        }
+      },
+      {
+        id: 2,
+        organizationId: 1,
+        name: 'McDonalds',
+        location: {
+          lat: 53.9208,
+          lng: 27.5600,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9208,
+            lng: 27.5600,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/mcdonalds.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'McDonalds'
+          }
+        }
+      },
+      {
+        id: 3,
+        organizationId: 1,
+        name: 'McDonalds',
+        location: {
+          lat: 53.9367,
+          lng: 27.5750,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9367,
+            lng: 27.5750,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/mcdonalds.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'McDonalds'
+          }
+        }
+      },
+      {
+        id: 4,
+        organizationId: 1,
+        name: 'McDonalds',
+        location: {
+          lat: 53.9017,
+          lng: 27.5680,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9017,
+            lng: 27.5680,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/mcdonalds.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'McDonalds',
+          }
+        }
+      },
+      {
+        id: 5,
+        organizationId: 1,
+        name: 'McDonalds',
+        location: {
+          lat: 53.9017,
+          lng: 27.5765,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9017,
+            lng: 27.5765,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/mcdonalds.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'McDonalds',
+          }
+        }
+      },
+      {
+        id: 6,
+        organizationId: 1,
+        name: 'McDonalds',
+        location: {
+          lat: 53.9207,
+          lng: 27.5885,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9207,
+            lng: 27.5885,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/mcdonalds.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'McDonalds'
+          }
+        }
+      },
+      {
+        id: 7,
+        organizationId: 1,
+        name: 'McDonalds',
+        location: {
+          lat: 53.9033,
+          lng: 27.5651,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9033,
+            lng: 27.5651,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/mcdonalds.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'McDonalds',
+          }
+        }
+      },
+      {
+        id: 8,
+        organizationId: 2,
+        name: 'KFC',
+        location: {
+          lat: 53.1147,
+          lng: 27.1680,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.1147,
+            lng: 27.1680,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/kfc.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'KFC'
+          }
+        }
+      },
+      {
+        id: 9,
+        organizationId: 2,
+        name: 'KFC',
+        location: {
+          lat: 53.5147,
+          lng: 27.2680,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.5147,
+            lng: 27.2680,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/kfc.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'KFC'
+          }
+        }
+      },
+      {
+        id: 10,
+        organizationId: 2,
+        name: 'KFC',
+        location: {
+          lat: 54.2147,
+          lng: 27.0680,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 54.2147,
+            lng: 27.0680,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/kfc.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'KFC'
+          }
+        }
+      },
+      {
+        id: 11,
+        organizationId: 2,
+        name: 'KFC',
+        location: {
+          lat: 54.9347,
+          lng: 28.5280,
+          country: 'Belarus',
+          city: 'Minsk',
+          marker: {
+            lat: 53.9347,
+            lng: 27.5280,
+            draggable: false,
+            iconAnchorX: 10,
+            iconAnchorY: 10,
+            iconHeight: 20,
+            iconWidth: 20,
+            icon: {
+              url: '/assets/restaurants-logo/kfc.png',
+              scaledSize: {
+                width: 30,
+                height: 30,
+              }
+            },
+            label: 'KFC'
+          }
+        }
+      },
+    ];
+
+    return {organizations, restaurants};
+  }
+}
--- cb-web-client/src/app/services/services.module.ts
+++ cb-web-client/src/app/services/services.module.ts
@@ -4,23 +4,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { MapSearchService } from './map-search.service';
 import { RestaurantService } from './restaurant.service';
 import { MessageService } from './message.service';
-import { InMemoryRestaurantService } from './in-memory/in-memory-restaurants.service';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './auth.service';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpModule } from '@angular/http';
+import { InMemoryDataService } from './in-memory/in-memory-data.service';
+import { OrganizationService } from './organization.service';
+import { RestaurantCommunicationService } from './restaurant-communication.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    HttpModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false },
+    ),
   ],
   providers: [
     MapSearchService,
+    RestaurantCommunicationService,
     RestaurantService,
+    OrganizationService,
     MessageService,
     AuthService,
-    InMemoryRestaurantService
+    InMemoryDataService
   ]
 })
 export class ServicesModule {
