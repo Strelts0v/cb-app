0ca44739614a2bc3b52ea27cdb5fd897f767ac14
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-01 22:53:31 +0300

    WC-3 - Update customer layout component decorator

--- cb-web-client/src/app/modules/layouts/customer-layout/customer-layout.component.ts
+++ cb-web-client/src/app/modules/layouts/customer-layout/customer-layout.component.ts
@@ -3,8 +3,7 @@ import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-customer-layout',
-  templateUrl: './customer-layout.component.html',
-  styleUrls: ['./customer-layout.component.scss']
+  templateUrl: './customer-layout.component.html'
 })
 export class CustomerLayoutComponent implements OnInit {
 
