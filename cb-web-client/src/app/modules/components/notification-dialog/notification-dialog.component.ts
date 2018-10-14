eb71f07709cc40776745b0c423b0b1584a0b0cdb
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-14 23:35:45 +0300

    WC-3 - Added notification dialog module

---
+++ cb-web-client/src/app/modules/components/notification-dialog/notification-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-notification-dialog',
+  templateUrl: './notification-dialog.component.html',
+  styleUrls: ['./notification-dialog.component.scss']
+})
+export class NotificationDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<NotificationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Object) {}
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+}
