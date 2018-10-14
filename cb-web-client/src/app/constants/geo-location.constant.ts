7bbb8de4851dfec8b383e73e721f914464ee726f
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-14 20:02:56 +0300

    WC-3 - Change default geo location data

--- cb-web-client/src/app/constants/geo-location.constant.ts
+++ cb-web-client/src/app/constants/geo-location.constant.ts
@@ -1,7 +1,7 @@
 export class GeoLocationData {
 
-    static readonly DEFAULT_LATITUDE = 75;
-    static readonly DEFAULT_LONGITUDE = 10;
+    static readonly DEFAULT_LATITUDE = 53.9347;
+    static readonly DEFAULT_LONGITUDE =  27.5280;
     static readonly DEFAULT_ZOOM = 13;
     static readonly DEFAULT_SEARCH_AREA_RADIUS = 2000;
     static readonly MILES_TO_RADIUS_RATIO = 0.00062137;
