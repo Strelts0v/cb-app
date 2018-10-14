9e78e8fc355582bd7593f2ed8416a27e53c53ee3
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-14 20:07:01 +0300

    WC-3 - Update restaurant communication service

--- cb-web-client/src/app/services/restaurant-communication.service.ts
+++ cb-web-client/src/app/services/restaurant-communication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Subject, BehaviorSubject } from 'rxjs';
 import { Organization } from '../data/organization';
 import { Restaurant } from '../data/restaurant';
 
@@ -9,17 +9,23 @@ import { Restaurant } from '../data/restaurant';
 export class RestaurantCommunicationService {
 
   // Observable sources
-  private organizationSource = new BehaviorSubject<Organization>(Organization.EMPTY_ORGANIZATION);
-  private restaurantsSource = new BehaviorSubject<Restaurant[]>([]);
+  private organizationSource = new Subject<Organization>();
+  private handledRestaurantSource = new Subject<Restaurant>();
+  private restaurantsSource = new Subject<Restaurant[]>();
 
   // Observable streams
   organization$ = this.organizationSource.asObservable();
+  handledRestaurant$ = this.handledRestaurantSource.asObservable();
   restaurants$ = this.restaurantsSource.asObservable();
 
   constructor() { }
 
   sendUpdatedOrganization(organization: Organization) {
     this.organizationSource.next(organization);
+  }
+
+  sendUpdatedHandledRestaurant(restaurant: Restaurant) {
+    this.handledRestaurantSource.next(restaurant);
   }
 
   sendUpdatedRestaurants(restaurants: Restaurant[]) {
