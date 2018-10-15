5d0aa5996e41b3c2ea79ccd554e4b446741a058d
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2018-10-15 06:02:24 +0300

    WC-5 - Refactor restaurant layout stuff

--- cb-web-client/src/app/modules/layouts/restaurant-layout/restaurant-layout.module.ts
+++ cb-web-client/src/app/modules/layouts/restaurant-layout/restaurant-layout.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { RestaurantLayoutRoutingModule } from './restaurant-layout-routing.module';
 import { RestaurantLayoutComponent } from './restaurant-layout.component';
 import { RestaurantControlModule } from '../../pages/restaurant-control/restaurant-control.module';
 import { MaterialModule } from '../../shared/material/material.module';
 import { FooterModule } from '../../components/footer/footer.module';
-import { RouterModule } from '@angular/router';
 import { RestaurantDetailsModule } from '../../pages/restaurant-details/restaurant-details.module';
 import { OrganizationDetailsModule } from '../../pages/organization-details/organization-details.module';
 
